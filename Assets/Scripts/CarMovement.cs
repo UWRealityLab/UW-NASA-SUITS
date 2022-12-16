fe4bdd7842df84f0461bcb5aa802b2ae654f305f
Sanjit Singh <ss3448@scarletmail.rutgers.edu>
2022-12-15 23:19:57 -0500

    making progess with rover movement

---
+++ Assets/Scripts/CarMovement.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour {
+
+    public float CarSpeed = 1f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+         
+    }
+}
