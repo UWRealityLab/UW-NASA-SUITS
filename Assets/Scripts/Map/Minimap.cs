20c93faad4dca7642ccf4700fab0da63cbf81022
Evan Zhao <zhw2513953778@outlook.com>
2023-02-22 16:27:05 -0800

    Added Minimap (for real this time)

--- Assets/Scripts/Map/Minimap.cs
+++ Assets/Scripts/Map/Minimap.cs
@@ -1,10 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
-public class Minimap : MonoBehaviour
+// This class takes all of the postiions of things in world coords,
+// and returns a list of tuples of everything to be rendered on a minimap
+// given a zoom, size, and numpixels (if you want 3d terrain)
+// You will need to pass this list to a renderer to actually show a minimap
+public class Minimap : MonoBehaviour 
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private Path activePath;
+    [SerializeField]
+    private WaypointManager waypoints;
+    [SerializeField]
+    private GameObject user;
+    [SerializeField]
+    private GameObject rover;
+
+    /*[SerializeField]
+    private GameObject pathMarkerPrefab;
+    [SerializeField]
+    private GameObject waypointMarkerPrefab;
+    [SerializeField]
+    private GameObject userMarkerPrefab;
+    [SerializeField]
+    private GameObject roverMarkerPrefab;*/
+
+
+    [SerializeField]
+    private float size; //size of the minimap in meter
+    [SerializeField]
+    private float zoom;
+    [SerializeField]
+    private float numPixels;
+
+    [SerializeField]
+    private Mesh terrain;
+
+    public List<Tuple<string, Vector3>> minimapList { get; }
     void Start()
     {
         
