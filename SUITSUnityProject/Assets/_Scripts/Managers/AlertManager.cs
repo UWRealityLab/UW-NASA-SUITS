92dddcc6e4092579d017ffbfae3f212d160ef3e2
wsheng24 <dcmenn@gmail.com>
2023-05-09 14:00:52 -0700

    Update AlertManager.cs

--- SUITSUnityProject/Assets/_Scripts/Managers/AlertManager.cs
+++ SUITSUnityProject/Assets/_Scripts/Managers/AlertManager.cs
@@ -4,5 +4,17 @@ using UnityEngine;
 
 public class AlertManager : Singleton<AlertManager>
 {
+    private Dictionary<TSSEVATypeEnum, TSSErrorStateEnum> _map = new ();
+    private Dictionary<TSSEVATypeEnum, TSSErrorStateEnum> _mapOld = new ();
 
+    private void Start()
+    {
+        TelemetryManager.OnBatteryPercentWarning += OnEVAErrorStateChange;
+        TelemetryManager.OnBatteryCapacityCaution += OnEVAErrorStateChange;
+    }
+
+    public void OnEVAErrorStateChange(string msg, TSSEVATypeEnum type)
+    {
+       /* if (_mapOld[type] != _map[type] && _mapOld[type])*/
+    }
 }
