a0ae56b662c89ba414df578105bf81fea7b761c0
ojasvikamboj <80231227+ojasvikamboj@users.noreply.github.com>
2023-05-19 16:55:18 -0700

    rover coord

---
+++ SUITSUnityProject/Assets/_Scripts/Rover_Coord_Input.cs
@@ -0,0 +1,54 @@
+using Microsoft.MixedReality.Toolkit.UX;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Rover_Coord_Input : MonoBehaviour
+{
+    [SerializeField] private MRTKTMPInputField _latitudeInput;
+    [SerializeField] private MRTKTMPInputField _longitudeInput;
+    [SerializeField] private MRTKTMPInputField _altInput;
+    [SerializeField] private MRTKTMPInputField _nameInput;
+
+    [SerializeField] private TMP_Text _detailsText;
+    [SerializeField] private TMP_Text _distanceText;
+
+    private Transform _user;
+    
+    
+    public void SendRover()
+    {
+       
+        RoverManager.Instance.SendRover(new Vector2(float.Parse(_latitudeInput.text), float.Parse(_longitudeInput.text)));
+    }
+
+    private void OnEnable()
+    {
+        ProcessCoordinate();
+        if (_user == null)
+            _user = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+        InvokeRepeating("UpdateDistance", 0, .25f);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+
+    private void ProcessCoordinate()
+    {
+        /* TODO: 1. Update the detailsText;
+         *       2. Update the distanceText
+                 3. Update the minimap*/
+        _detailsText.text = $"Details:\n" +
+            $"N {_latitudeInput.text}'\n" +
+            $"W {_longitudeInput.text}'\n" +
+             $"Alt : {_altInput.text}'\n";
+    }
+
+    private void UpdateDistance()
+    {
+        _distanceText.text = $"{ ((_user.position - GPSHandler.Instance.GPStoWorld(new Vector2(float.Parse(_latitudeInput.text), float.Parse(_longitudeInput.text)))).magnitude).ToString("G3")} m away";
+    }
+}
