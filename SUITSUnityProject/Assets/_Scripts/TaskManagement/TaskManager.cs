cc90bc456731dfbaf85aaf8b387253376cff5ca5
DavidMenn <dcmenn@gmail.com>
2023-05-19 17:54:46 -0700

    Added tasks

--- SUITSUnityProject/Assets/_Scripts/TaskManagement/TaskManager.cs
+++ SUITSUnityProject/Assets/_Scripts/TaskManagement/TaskManager.cs
@@ -18,30 +18,26 @@ public class TaskManager : Singleton<TaskManager>
     private string taskFilePath;
     void Start()
     {
-        taskFilePath = Application.dataPath + "/TaskFile.json";
-        tasklist = ReadJson();
-        /*tasklist = new List<TaskObject>();
+        //taskFilePath = Application.dataPath + "/TaskFile.json";
+        //tasklist = ReadJson();
+        tasklist = new List<TaskObject>();
         tasklist.Add(new TaskObject());
-        tasklist[0].shortTask = "Finish making task manager";
-        tasklist[0].longTask = "Your role in completing the Task Manager involves finalizing the user interface, implementing the remaining functionality, conducting thorough testing, and ensuring the application is stable and optimized for the HoloLens platform. Additionally, you will need to document the code and provide clear instructions for users to navigate and utilize the Task Manager effectively.\n"+
-                                "By successfully completing this project, you will contribute to enhancing task management efficiency and user experience in the augmented reality space, empowering HoloLens users to seamlessly organize and track their tasks in a visually engaging and immersive manner.";
+        tasklist[0].shortTask = "UIA Egress";
+        tasklist[0].longTask = "Open up the UIA egress screen from the top buttons on the dashboard or the hand menu. There it should show you which switches are flipped in the wrong position. You should then work to flip the indicated switch to the correct position.";
         tasklist[0].index = 0;
         tasklist.Add(new TaskObject());
-        tasklist[1].shortTask = "Go frolick in a pile of rocks in Houston";
-        tasklist[1].longTask = "The task \"Go frolick in a pile of rocks in Houston\" is a delightful adventure that invites you to immerse yourself in a playful and whimsical experience.Set in the vibrant city of Houston, this task encourages you to embrace your inner child and indulge in the simple joys of life." +
-            "Houston, known for its diverse landscapes, offers a unique opportunity to find a pile of rocks and turn it into your personal playground.As you frolic amidst the rocks, you can let your imagination run wild, creating stories and scenarios, relishing the freedom and carefree spirit of the moment.";
+        tasklist[1].shortTask = "Geological Scan";
+        tasklist[1].longTask = "Press the spectroscopy button on top of the dashboard, or open it from the hand menu. Then you will see the spectroscopy window pop up. You can then press past spectroscopies to look at the past samples. The latest scan should show up and you can select one to look at the data from the latest scan (which will be the last one in the list)";
         tasklist[1].index = 1;
         tasklist.Add(new TaskObject());
-        tasklist[2].shortTask = "Eat free food";
-        tasklist[2].longTask = "The task \"Go frolick in a pile of rocks in Houston\" is a delightful adventure that invites you to immerse yourself in a playful and whimsical experience.Set in the vibrant city of Houston, this task encourages you to embrace your inner child and indulge in the simple joys of life." +
-            "Houston, known for its diverse landscapes, offers a unique opportunity to find a pile of rocks and turn it into your personal playground.As you frolic amidst the rocks, you can let your imagination run wild, creating stories and scenarios, relishing the freedom and carefree spirit of the moment.";
+        tasklist[2].shortTask = "Navigate Rover";
+        tasklist[2].longTask = "Listen to the test coordinator as they tell you which points of interest to navigate the rover to. You can press Navigate Rover on the navigation panel of the dashboard, then navigate to saved waypoints, then press the waypoint of the point of interest you are asked to navigate to and press send rover.";
         tasklist[2].index = 2;
         tasklist.Add(new TaskObject());
-        tasklist[3].shortTask = "Task: Write Poor Quality Code Description: Your task is to deliberately write poorly structured and inefficient code. Embrace practices that violate industry standards and best practices, aiming to create code that is difficult to read, understand, and maintain. Consider the following guidelines: 1. Ignore code organization: Avoid using functions or classes, and keep all code in a single file. 2. Use cryptic variable and function names: Opt for names that provide no meaningful context or description. 3. Avoid commenting: Omit any comments or documentation, making it challenging for others to comprehend your code's purpose. 4. Neglect error handling: Ignore potential errors or exceptions, allowing them to propagate unhandled. 5. Disregard code optimization: Prioritize writing code quickly over efficiency, resulting in poor performance and excessive resource consumption. Remember, the goal is to produce code that is convoluted, hard to maintain, and prone to errors.";
-        tasklist[3].longTask = "The task \"Go frolick in a pile of rocks in Houston\" is a delightful adventure that invites you to immerse yourself in a playful and whimsical experience.Set in the vibrant city of Houston, this task encourages you to embrace your inner child and indulge in the simple joys of life." +
-            "Houston, known for its diverse landscapes, offers a unique opportunity to find a pile of rocks and turn it into your personal playground.As you frolic amidst the rocks, you can let your imagination run wild, creating stories and scenarios, relishing the freedom and carefree spirit of the moment.";
+        tasklist[3].shortTask = "Navigate Home";
+        tasklist[3].longTask = "Use the breadcrumb trail to return home. Press on navigate astronaut, then return home. Leave Track Back highlighted, then press navigate. A trail home should appear in front of your, showing the path that you took back. You are welcome to take an easier path if you see fit, but this will help you not get lost!";
         tasklist[3].index = 3;
-        activeTask = 1;*/
+        activeTask = 0;
 
     }
 
